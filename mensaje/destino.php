3bc1a012c2c22b76579d3b06c08e38d84dc62505
cuellaralan <alan.javier.ajc@gmail.com>
2016-08-30 21:48:59 -0300

    superficie rectangulo

---
+++ mensaje/destino.php
@@ -0,0 +1,47 @@
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso Ingreso UTN FRA</title>
+
+		<!--Estilos-->
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<link rel="stylesheet" type="text/css" href="css/animations.css">
+		<!--final de Estilos-->
+
+		<!--Lógica-Programación-->
+
+        <!--Final de Lógica-Programación -->
+
+
+	</head>
+	<body>
+
+		
+		<div class="CajaUno">
+			
+
+				<?php 
+				
+					$base= $_POST['base'];
+					$alt=$_POST['altura'];
+					$sup =  $base * $alt;
+
+					echo "la superficie es: ".$sup;
+				 ?>
+
+		</div>
+
+		<center>
+
+		</center>
+
+	</body>
+</html>
+
+
+
+
